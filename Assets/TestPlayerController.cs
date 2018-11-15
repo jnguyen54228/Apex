fe9e7931d1237562e82b2787623f7f9f20e60f83
John Nguyen <nguyenduyjohn180@gmail.com>
2018-11-15 13:35:26 -0800

    added to test scene

--- Assets/TestPlayerController.cs
+++ Assets/TestPlayerController.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class TestPlayerController : MonoBehaviour {
+public class TestPlayerController : NetworkBehaviour {
 
-	// Use this for initialization
+    [SyncVar]
+    public bool button1Press = false;
+    [SyncVar]
+    public bool button2Press = false;
+
 	void Start () {
-		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+        if (button1Press == true)
+        {
+            GameObject.Find("Cube 1").transform.position = new Vector3(-3, 3, 0);
+        }
+        else if (button2Press == true)
+        {
+            GameObject.Find("Cube 2").transform.position = new Vector3(3, 3, 0);
+        }
 	}
+
+    public void ButtonPress1()
+    {
+        button1Press = true;
+    }
+
+    public void ButtonPress2()
+    {
+        button2Press = true;
+    }
 }
