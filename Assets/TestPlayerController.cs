5b75578fc12259041ca5e8fc1be3b6bce1feb022
John Nguyen <nguyenduyjohn180@gmail.com>
2018-11-25 21:56:53 -0800

    commands and rpcs to test scene

--- Assets/TestPlayerController.cs
+++ Assets/TestPlayerController.cs
@@ -5,32 +5,48 @@ using UnityEngine.Networking;
 
 public class TestPlayerController : NetworkBehaviour {
 
-    [SyncVar]
-    public bool button1Press = false;
-    [SyncVar]
-    public bool button2Press = false;
-
-	void Start () {
-	}
-	
-	void Update () {
-        if (button1Press == true)
+    void Start() {
+        if (!isServer)
         {
-            GameObject.Find("Cube 1").transform.position = new Vector3(-3, 3, 0);
+            CmdChangeName("Player 2");
         }
-        else if (button2Press == true)
-        {
-            GameObject.Find("Cube 2").transform.position = new Vector3(3, 3, 0);
+        else if (isServer) {
+            CmdChangeName("Player 1");
         }
-	}
 
-    public void ButtonPress1()
-    {
-        button1Press = true;
     }
 
-    public void ButtonPress2()
+    void Update() {
+        if (TestCubeScript.button1Press == true) {
+            if (!isServer) {
+                CmdMoveCube(new Vector2(transform.position.x, transform.position.y + 1));
+                TestCubeScript.button1Press = false;
+            }
+            else {
+                RpcUpdateCube(new Vector2(transform.position.x, transform.position.y + 1));
+                TestCubeScript.button1Press = false;
+            }
+        }
+    }
+
+    [Command]
+    void CmdMoveCube(Vector2 pos) {
+        RpcUpdateCube(pos);
+    }
+
+    [ClientRpc]
+    void RpcUpdateCube(Vector2 pos)
     {
-        button2Press = true;
+        transform.position = pos;
+    }
+
+    [Command]
+    void CmdChangeName(string name) {
+        RpcUpdateName(name);
+    }
+
+    [ClientRpc]
+    void RpcUpdateName(string name) {
+        transform.name = name;
     }
 }
